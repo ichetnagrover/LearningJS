b04fcfe10e740768e5ac05a7afbb450d5e3ab2b1
ichetnagrover <groverchetna0@gmail.com>
2024-01-22 20:06:37 +0530

    variables practice

---
+++ variables.js
@@ -0,0 +1,27 @@
+// Create a variable called carName, assign the value Volvo to it.
+let carName= "Volvo";
+
+// Create a variable called x, assign the value 50 to it.
+let x = 50;
+
+// Display the sum of 5 + 10, using two variables: x and y.
+let x =5 ;
+let y = 10;
+document.getElementById("demo").innerHTML = x+y;
+
+// Create a variable called z, assign x + y to it, and display the result in an alert box.
+let x = 5;
+let y = 10;
+let x= x + y;
+alert (z);
+
+// On one single line, declare three variables with the following names and values:
+
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+
+let firstName = "John", lastName = "Doe", age = 35 ;
+
+
